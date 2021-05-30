cd2b5c7247fb640ebce1d1e8965ae1261eadd638
BradyBlackley <bradyblackley@mail.weber.edu>
2021-05-29 17:08:15 -0700

    updated app and connected the repositories, services and the view

--- src/main/java/learn/dontwreckmyhouse/App.java
+++ src/main/java/learn/dontwreckmyhouse/App.java
@@ -1,7 +1,37 @@
 package learn.dontwreckmyhouse;
 
+import learn.dontwreckmyhouse.data.GuestFileRepository;
+import learn.dontwreckmyhouse.data.HostFileRepository;
+import learn.dontwreckmyhouse.data.ReservationFileRepository;
+import learn.dontwreckmyhouse.domain.GuestService;
+import learn.dontwreckmyhouse.domain.HostService;
+import learn.dontwreckmyhouse.domain.ReservationService;
+import learn.dontwreckmyhouse.ui.ConsoleIO;
+import learn.dontwreckmyhouse.ui.Controller;
+import learn.dontwreckmyhouse.ui.View;
+
 public class App {
     public static void main(String[] args) {
+
+        GuestFileRepository guestRepository
+                = new GuestFileRepository("./data/app-data/guests.csv");
+        HostFileRepository hostRepository
+                = new HostFileRepository("./data/app-data/hosts.csv");
+        ReservationFileRepository reservationRepository
+                = new ReservationFileRepository("./data/app-data/reservations",
+                guestRepository, hostRepository);
+
+        GuestService guestService =
+                new GuestService(guestRepository);
+        HostService hostService =
+                new HostService(hostRepository);
+        ReservationService reservationService =
+                new ReservationService(reservationRepository, guestRepository, hostRepository);
+
+        ConsoleIO io = new ConsoleIO();
+        View view = new View(io);
+        Controller controller = new Controller(guestService, hostService, reservationService, view);
+        controller.run();
 
     }
 }
--- src/main/java/learn/dontwreckmyhouse/ui/Controller.java
+++ src/main/java/learn/dontwreckmyhouse/ui/Controller.java
@@ -1,7 +1,52 @@
 package learn.dontwreckmyhouse.ui;
 
+import learn.dontwreckmyhouse.data.DataException;
+import learn.dontwreckmyhouse.domain.GuestService;
+import learn.dontwreckmyhouse.domain.HostService;
+import learn.dontwreckmyhouse.domain.ReservationService;
+
 public class Controller {
 
+    private final GuestService guestService;
+    private final HostService hostService;
+    private final ReservationService reservationService;
+    private final View view;
+
+    public Controller(GuestService guestService, HostService hostService,
+                      ReservationService reservationService, View view) {
+        this.guestService = guestService;
+        this.hostService = hostService;
+        this.reservationService = reservationService;
+        this.view = view;
+    }
+
+    public void run() {
+        view.displayHeader("Welcome to Don't Wreck My House!");
+        try {
+            runAppLoop();
+        } catch (DataException ex) {
+            view.displayException(ex);
+        }
+        view.displayHeader("Goodbye.");
+    }
+
+    private void runAppLoop() throws DataException {
+        MainMenuOption option;
+        do {
+            option = view.selectMainMenuOption();
+            switch (option) {
+                case VIEW_RESERVATIONS_FOR_HOST :
+                    break;
+                case MAKE_A_RESERVATION :
+                    break;
+                case EDIT_A_RESERVATION :
+                    break;
+                case CANCEL_A_RESERVATION :
+                    break;
+
+            }
+        } while (option != MainMenuOption.EXIT);
+    }
 
 
 }
